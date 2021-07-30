ffe59a72dbc3f613a04706271a0c2ffee4793db8
Guilherme F. Trindade <38335563+GuilhermeFT@users.noreply.github.com>
2021-07-30 19:45:43 -0300

    carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
 import { Carousel } from './components/Carousel'
 
+import './styles/global.scss'
+
 function App() {
-  return <Carousel />
+  return (
+    <Carousel>
+      <div style={{ height: '100%', width: '100%', backgroundColor: 'green' }}>
+        <h2>Banner 1</h2>
+      </div>
+      <div style={{ height: '100%', width: '100%', backgroundColor: 'red' }}>
+        <h2>Banner 2</h2>
+      </div>
+      <div style={{ height: '100%', width: '100%', backgroundColor: 'yellow' }}>
+        <h2>Banner 3</h2>
+      </div>
+      <div style={{ height: '100%', width: '100%', backgroundColor: 'pink' }}>
+        <h2>Banner 4</h2>
+      </div>
+    </Carousel>
+  )
 }
 
 export default App
