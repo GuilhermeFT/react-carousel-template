cf448d9c80d32e9843a392e970b528b093dbb059
Guilherme F. Trindade <38335563+GuilhermeFT@users.noreply.github.com>
2021-08-03 09:44:51 -0300

    carousel test

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,6 @@ function App() {
       <div style={{ height: '80%', width: '80%', backgroundColor: 'green' }}>
         <h2>Banner 1</h2>
       </div>
-      <div style={{ height: '80%', width: '80%', backgroundColor: 'red' }}>
-        <h2>Banner 2</h2>
-      </div>
-      <div style={{ height: '80%', width: '80%', backgroundColor: 'yellow' }}>
-        <h2>Banner 3</h2>
-      </div>
-      <div style={{ height: '80%', width: '80%', backgroundColor: 'pink' }}>
-        <h2>Banner 4</h2>
-      </div>
     </Carousel>
   )
 }
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable indent */
+/* eslint-disable multiline-ternary */
 import { ReactNode, useState } from 'react'
 
 import styles from './styles.module.scss'
 
 interface CarouselProps {
-  children: ReactNode[]
+  children: ReactNode[] | ReactNode
 }
 export function Carousel({ children }: CarouselProps) {
   const numberOfChildren = children.length
@@ -25,23 +27,33 @@ export function Carousel({ children }: CarouselProps) {
   }
   return (
     <div className={styles.container}>
-      <button
-        aria-label="Voltar para o banner anterior"
-        onClick={handleToPrevious}
-      >
-        <svg viewBox="0 0 451.847 451.847">
-          <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z" />
-        </svg>
-      </button>
-      {children.map((child, index) => (index === currentChild ? child : null))}
-      <button
-        aria-label="Avançar para o banner posterior"
-        onClick={handleToNext}
-      >
-        <svg viewBox="0 0 451.847 451.847">
-          <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z" />
-        </svg>
-      </button>
+      {numberOfChildren > 1 ? (
+        <button
+          aria-label="Voltar para o banner anterior"
+          onClick={handleToPrevious}
+        >
+          <svg viewBox="0 0 451.847 451.847">
+            <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z" />
+          </svg>
+        </button>
+      ) : null}
+
+      {numberOfChildren > 1
+        ? children.map((child, index) =>
+            index === currentChild ? child : null
+          )
+        : children}
+
+      {numberOfChildren > 1 ? (
+        <button
+          aria-label="Avançar para o banner posterior"
+          onClick={handleToNext}
+        >
+          <svg viewBox="0 0 451.847 451.847">
+            <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z" />
+          </svg>
+        </button>
+      ) : null}
     </div>
   )
 }
